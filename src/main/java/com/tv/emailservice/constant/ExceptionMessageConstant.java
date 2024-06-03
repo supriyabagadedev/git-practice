e107358f72dfe2b7b60075f080f14e04488a7887
Supriya Bagade <supriya.bagade@techerito.com>
2024-06-03 15:04:17 +0530

    main commit 2

--- src/main/java/com/tv/emailservice/constant/ExceptionMessageConstant.java
+++ src/main/java/com/tv/emailservice/constant/ExceptionMessageConstant.java
@@ -2,4 +2,5 @@ package com.tv.emailservice.constant;
 
 public class ExceptionMessageConstant {
     public static final String INVALID_NOTIFICATION_TYPE_SPECIFIED = "Invalid notification type specified";
+    public static final String CONSTANT = "constant";
 }
